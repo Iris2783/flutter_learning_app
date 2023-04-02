6965a6158b2d903425eafc3ac507f92f9a859e91
Iris2783 <iris.corp.2783@gmail.com>
2023-04-02 21:58:54 +0900

    Add Splash Screen

--- flutter_learning_app/lib/main.dart
+++ flutter_learning_app/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_learning_app/loginpage.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized(); //Widgetsバインディグの初期化　アプリを正常に動かすために必要な処理
+  await Future.delayed(const Duration(seconds: 10)); //起動の処理が完了するまで10秒待機　その間スプラッシュ画面を表示
+  FlutterNativeSplash.remove(); //スプラッシュ画面を削除
   runApp(const MyApp());
 }
 
@@ -10,8 +14,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: LoginPage(),
+    return MaterialApp(
+      title: 'Frawers',
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
+      home: const LoginPage(),
     );
   }
 }
